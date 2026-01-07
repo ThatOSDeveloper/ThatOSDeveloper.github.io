b6765c8d66b4399847ff02a092fb60e0e6b0bf43
ThatOSDeveloper <owen.winchell@proton.me>
2026-01-07 00:21:02 -0500

    Fixed  RSS?

---
+++ assets/js/zc-popup.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  loadZCPopup(
+    '3z52116be31c38cbb40ffda027ff1171c7f72b850dc8eb63b3d2affcba9304cb54',
+    'ZCFORMVIEW',
+    '3z7c3513bba38f22973d70a6f6d3f675ea'
+  );
+};
